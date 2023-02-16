255b839ffa4e90bcae3672199ca6458c89e90ed0
Yub_2 <hyeok230@naver.com>
2023-02-16 14:28:26 +0900

    Time: 7 ms, Memory: 10.7 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        std::unordered_map<int, int> um;
-        vector<int> ret;
-        for (int i = 0; i < nums1.size(); i++)
+        if (nums1.size() > nums2.size())
+            swap(nums1, nums2);
+    
+        unordered_map<int, int> um;
+        for (auto n:nums1)
+            um[n]++;
+        
+        int k = 0;
+        for (auto n:nums2)
         {
-            if (um[nums1[i]])
-                um[nums1[i]]++;
-            else
-                um[nums1[i]] = 1;
-        }
-        for (int i = 0; i < nums2.size(); i++)
-        {
-            if (um[nums2[i]])
+            if (um[n] > 0)
             {
-                um[nums2[i]]--;
-                ret.push_back(nums2[i]);
+                um[n]--;
+                nums1[k++] = n;
             }
         }
-        return ret;
+        return vector<int>(nums1.begin(), nums1.begin()+k);
     }
 };
