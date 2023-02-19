c362c579bd3715c4527182f83f319ee012b8be48
Yub_2 <hyeok230@naver.com>
2023-02-19 21:56:57 +0900

    Time: 12 ms, Memory: 7.4 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int list[26];
-        bzero(list, sizeof(list));
-        for (int i = 0; i < s.size(); i++)
-        {
-            int index = s[i] - 'a';
-            list[index]++;
-        }
-        for (int i = 0; i < t.size(); i++)
-        {
-            int index = t[i] - 'a';
-            list[index]--;
-        }
-        for (int i = 0; i < 26; i++)
-        {
-            if (list[i] != 0)
-                return false;
-        }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s != t)
+            return false;
         return true;
     }
 };
