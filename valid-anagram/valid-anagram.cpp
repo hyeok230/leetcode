a750765afbdc1150a9e956c2c013424ec8eca9d5
Yub_2 <hyeok230@naver.com>
2023-02-19 22:37:04 +0900

    Time: 9 ms, Memory: 7.3 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s != t)
-            return false;
+        if (s.size() != t.size()) return false;
+        int n = s.size();
+        unordered_map<char, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[s[i]]++;
+            m[t[i]]--;
+        }
+        for (auto count: m)
+            if (count.second) return false;
         return true;
     }
 };
