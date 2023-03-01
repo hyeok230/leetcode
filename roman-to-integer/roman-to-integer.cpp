677e31a0f2ea54e8dc7432bfd2e0d19632afbe16
Yub_2 <hyeok230@naver.com>
2023-03-01 16:44:54 +0900

    Time: 20 ms, Memory: 7.7 MB - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -1,67 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int n = 0;
-        queue<char> q;
-        for (int i = 0; i < s.size(); i++)
-            q.push(s[i]);
-        while (!q.empty())
+        unordered_map<char, int> m = {{'I' , 1},
+                                      {'V' , 5},
+                                      {'X' , 10},
+                                      {'L' , 50},
+                                      {'C' , 100},
+                                      {'D' , 500},
+                                      {'M' , 1000}};
+        int sum = m[s.back()];
+        for (int i = s.size() - 2; i >= 0; i--)
         {
-            char c = q.front();
-            q.pop();
-            if (c == 'I')
+            if (m[s[i]] < m[s[i + 1]])
             {
-                if (q.front() == 'V')
-                {
-                    q.pop();
-                    n += 4;
-                } else if (q.front() == 'X')
-                {
-                    q.pop();
-                    n += 9;
-                }
-                else 
-                    n += 1;
+                sum -= m[s[i]];
+            } else {
+                sum += m[s[i]];
             }
-            else if (c == 'V')
-                n += 5;
-            else if (c == 'X')
-            {
-                if (q.front() == 'L')
-                {
-                    q.pop();
-                    n += 40;
-                } else if (q.front() == 'C')
-                {
-                    q.pop();
-                    n += 90;
-                } else {
-                    n += 10;
-                }
-            }
-            else if (c == 'L')
-                n += 50;
-            else if (c == 'C')
-            {
-                if (q.front() == 'D')
-                {
-                    q.pop();
-                    n += 400;
-                } else if (q.front() == 'M')
-                {
-                    q.pop();
-                    n += 900;
-                } else {
-                    n += 100;
-                }
-            }
-            else if (c == 'D')
-                n += 500;
-            else if (c == 'M')
-                n += 1000;
         }
-        
-        
-        return n;
+        return sum;
     }
 };
