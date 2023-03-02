46424719ebd26b372e5f1bec11fc4212437314dd
Yub_2 <hyeok230@naver.com>
2023-03-02 16:35:48 +0900

    Time: 4 ms, Memory: 6.4 MB - LeetHub

--- valid-parentheses/valid-parentheses.cpp
+++ valid-parentheses/valid-parentheses.cpp
@@ -4,29 +4,16 @@ public:
         stack<char> st;
         for (int i = 0; i < s.size(); i++)
         {
-            if (s[i] == ')')
-            {
-                if (!st.empty() && st.top() == '(')
-                    st.pop();
-                else
-                    return false;
-            } else if (s[i] == '}')
-            {
-                if (!st.empty() && st.top() == '{')
-                    st.pop();
-                else
-                    return false;
-            } else if (s[i] == ']')
-            {
-                if (!st.empty() && st.top() == '[')
-                    st.pop();
-                else 
-                    return false;
-            } else 
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
                 st.push(s[i]);
+            else {
+                if (st.empty()) return false;
+                if (s[i] == ')' && st.top() != '(') return false;
+                if (s[i] == ']' && st.top() != '[') return false;
+                if (s[i] == '}' && st.top() != '{') return false;
+                st.pop();
+            }
         }
-        if (!st.empty())
-            return false;
-        return true;
+        return st.empty();
     }
 };
