e7fce203485ef5ea61da7d8513466f3dd73d733a
Yub_2 <hyeok230@naver.com>
2023-03-02 17:38:53 +0900

    Time: 18 ms, Memory: 17.9 MB - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int bit = n;
+        int bit = 0;
         for (int i = 0; i < n; i++)
         {
             bit ^= nums[i];
             bit ^= i;
         }
+        bit ^= n;
         return bit;
     }
 };
